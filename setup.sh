1acf74805e99293b23f109d3ada41edb2c019b07
SSTL-Drone <sstldrone@gmail.com>
2023-09-15 15:17:46 -0500

    Install dronekit from git repo

--- setup.sh
+++ setup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+
 pip install lxml==4.8.0
-pip install dronekit dronekit-sitl pymavlink pytest depthai-sdk gpxpy websockets asyncio
+pip install git+https://github.com/dronekit/dronekit-python.git
+pip install dronekit-sitl pymavlink pytest depthai-sdk gpxpy websockets asyncio
 
 # Might be needed?
 bash -c "$(curl -fL https://docs.luxonis.com/install_dependencies.sh)"
